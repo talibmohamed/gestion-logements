6bbd17336688417e0fe4b7b760d488ab3c8e8e61
Talib Mohamed <talibmed11@gmail.com>
2024-04-28 23:18:02 +0100

    final layout for login page, still need some adjustment

--- FrontEnd/src/componants/login/login.jsx
+++ FrontEnd/src/componants/login/login.jsx
@@ -1,69 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
 import house from "./house.jpg";
-import "./login.css";
+import "./login.scss";
+import { Form, Button } from "react-bootstrap";
 
-function SignIn() {
+const SignIn = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your sign-in logic here
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
   return (
     <>
-      <div className="row">
-        <div className="col-sm-4 col-xs-12 position-fixed top-0 start-0 h-100 rounded-start-5 ">
-          <div className="row">
-            <div className="col d-flex align-items-center justify-content-center title">
-              <img className="img-fluid" src={logo} />
-              <p className="houselytics mb-0 ml-2">houselytics</p>
+      <div className="container-fluid">
+        <div className="row page">
+          <div className="col-sm-12 col-md-4 full-height stack-on-small">
+            <div className="row justify-content-center align-items-center">
+              <div className="row align-items-center justify-content-center title">
+                <div className="col-auto p-0">
+                  <img className="img-fluid" src={logo} alt="Logo" />
+                </div>
+                <div className="col-auto p-0">
+                  <p className="houselytics mb-0">Houselytics</p>
+                </div>
+              </div>
+              <div className="row p-0 m-0">
+                <img
+                  src={house}
+                  className="img-fluid align-content-center p-0"
+                  alt="House"
+                />
+              </div>
             </div>
           </div>
-          <div className="row">
-            <img
-              src={house}
-              className="img-fluid rounded-start-5 align-content-center "
-            />
-          </div>
-        </div>
-        <div
-          className="col-sm-8 col-xs-12 position-fixed top-0 end-0 h-100  rounded-start-5 "
-          style={{ backgroundColor: "white" }}
-        >
-          <div className="row">
-            <div className="col d-flex align-items-center justify-content-center">
-              <p className="login">Login</p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col d-flex align-items-center justify-content-center">
-              <input type="text" className="form-control" placeholder="Email" />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col d-flex align-items-center justify-content-center">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col d-flex align-items-center justify-content-center">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col d-flex align-items-center justify-content-center">
-              <p className="forgot">Forgot Password?</p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col d-flex align-items-center justify-content-center">
-              <p className="forgot">Don't have an account? Sign Up</p>
-            </div>
+          <div className="col-sm-12 col-md-8 d-flex justify-content-center align-items-center flex-column right">
+            <h2 className="header">Sign In Account</h2>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="username-group" controlId="formUsername">
+                <Form.Label>Username or Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter username or email"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="password-group" controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <div className="button-container">
+                {" "}
+                {/* Container for centering the button */}
+                <Button className="button" variant="primary" type="submit">
+                  Sign In
+                </Button>
+              </div>
+            </Form>
+            <a href="#">Forgot password?</a>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default SignIn;
